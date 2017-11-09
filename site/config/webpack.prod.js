4030a254ce5c2b7ba4849fd9e6a4eb85b242f22b
nodasglt <ndsglt@gmail.com>
2017-11-09 10:53:10 +0200

    server cleanup: no render, only google & local auth

--- site/config/webpack.prod.js
+++ site/config/webpack.prod.js
@@ -66,7 +66,7 @@ module.exports = function (env) {
        *
        * See: http://webpack.github.io/docs/configuration.html#output-path
        */
-      path: helpers.root('dist'),
+      path: helpers.root('../docker/kong/www'),
 
       /**
        * Specifies the name of each output file on disk.
