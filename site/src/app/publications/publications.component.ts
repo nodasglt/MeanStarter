4bc6de07c0d71982514dc4410e9c2f66e96786c7
nodasglt <ndsglt@gmail.com>
2017-11-09 10:38:25 +0200

    Publications page

---
+++ site/src/app/publications/publications.component.ts
@@ -0,0 +1,11 @@
+import {
+  Component,
+} from '@angular/core';
+
+@Component({
+  selector: 'publications',
+  styleUrls: ['../landing-page/landing-page.component.scss', 'publications.component.scss'],
+  templateUrl: 'publications.component.html'
+})
+export class PublicationsComponent {
+}
