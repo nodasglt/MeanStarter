d8e2a62452aedacf870c914d350e6c7691f72912
nodasglt <ndsglt@gmail.com>
2017-11-09 10:38:52 +0200

    Team page

---
+++ site/src/app/team/team.component.ts
@@ -0,0 +1,12 @@
+import {
+  Component,
+} from '@angular/core';
+
+@Component({
+  selector: 'team',
+  styleUrls: [ '../landing-page/landing-page.component.scss', 'team.component.scss' ],
+  templateUrl: 'team.component.html'
+})
+export class TeamComponent {
+    desc = "Sure ipsizzle dolor sit boofron, consectetuer adipiscing pot. Nullizzle sapizzle velit, boofron volutpizzle, suscipit quis, gravida vizzle, dope. That's the shizzle gangster tortizzle. Sure erizzle. Boofron izzle dolizzle dapibizzle sizzle tempizzle tempor. Maurizzle pellentesque nibh izzle sheezy. I saw beyonces tizzles and my pizzle went crizzle izzle tortizzle. Brizzle mammasay mammasa mamma oo sa rhoncizzle yippiyo. In hac habitasse platea dictumst. Bling bling dapibizzle.";
+}
