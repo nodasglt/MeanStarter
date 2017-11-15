05c0d5bc1950d9e1739940287b521ddf38bd6399
nodasglt <ndsglt@gmail.com>
2017-11-15 09:04:05 +0200

    Admin page base

---
+++ site/src/app/admin/profile/profile.component.ts
@@ -0,0 +1,46 @@
+import {
+    Router,
+    ActivatedRoute,
+    Params
+} from '@angular/router';
+import {
+    OnInit,
+    Component
+} from '@angular/core';
+import {
+    AuthService
+} from '../../core';
+import {
+    Observable
+} from 'rxjs/Rx';
+
+@Component({
+    selector: 'profile',
+    templateUrl: 'profile.component.html',
+    styleUrls: ['profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private authService: AuthService,
+    ) {
+    }
+
+    token: string = '';
+    test: Observable<string>;
+
+    ngOnInit() {
+        // subscribe to router event
+        this.activatedRoute.queryParams.subscribe((params: Params) => {
+            const code = params['code'];
+            if (code) {
+                this.authService.loginGoogle(code, false).subscribe(_ => {
+                    console.log(_);
+                    this.token = this.authService.getToken();
+                    this.test = this.authService.test();
+                })
+            }
+        });
+    }
+}
