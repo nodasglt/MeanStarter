3e9b0f016992fff64b4ca84ad68bd46a690714f2
nodasglt <ndsglt@gmail.com>
2017-11-15 09:00:56 +0200

    Added footer component

---
+++ site/src/app/footer-bar/footer-bar.component.ts
@@ -0,0 +1,11 @@
+import {
+  Component,
+} from '@angular/core';
+
+@Component({
+  selector: 'footer-bar',
+  styleUrls: [ 'footer-bar.component.scss' ],
+  templateUrl: 'footer-bar.component.html'
+})
+export class FooterBarComponent {
+}
---
+++ site/src/app/footer-bar/index.ts
@@ -0,0 +1,1 @@
+export * from './footer-bar.component';
